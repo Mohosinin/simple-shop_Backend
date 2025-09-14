1191181924c4b022cef55ac20e5310ecf3df8005
Mohosinin <mohosinin.islam34@gmail.com>
2025-09-14 21:52:27 +0600

    noting

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1:5500'];
 
+
 app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin like Postman, curl or server-to-server
